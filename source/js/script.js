a3dd2d3f03a72bf4c430c58eeaf596e6cb588372
CrashStatic <thecrashstatic@gmail.com>
2023-11-23 19:17:10 +0300

    Работает с навигацией

---
+++ source/js/script.js
@@ -0,0 +1,12 @@
+let navMain = document.querySelector('.navigation');
+let navToggle = document.querySelector('.navigation__toggle');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('navigation--closed')) {
+    navMain.classList.remove('navigation--closed');
+    navMain.classList.add('navigation--opened');
+  } else {
+    navMain.classList.add('navigation--closed');
+    navMain.classList.remove('navigation--opened');
+  }
+});
