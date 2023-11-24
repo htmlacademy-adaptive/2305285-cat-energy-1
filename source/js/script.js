ff5a68b4db12d9c0531e9177c264780846b560c1
CrashStatic <thecrashstatic@gmail.com>
2023-11-24 11:37:11 +0300

    Добавляет стили для навигации при выключенном JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,8 @@
 let navMain = document.querySelector('.navigation');
 let navToggle = document.querySelector('.navigation__toggle');
 
+navMain.classList.remove('navigation--nojs');
+
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('navigation--closed')) {
     navMain.classList.remove('navigation--closed');
